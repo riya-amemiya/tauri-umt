38bcea7343907e51ecb258ad989586d78bf8dbbf
Yuta Nishi <riyaamemiya@gmail.com>
2024-10-03 19:29:42 +0900

    chore: index.scssを削除し、関連インポートをindex.tsxから削除

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -4,23 +4,18 @@ import { useCallback, useMemo, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { generateApiInstance } from "@/utils/generateApiInstance";
 
-import "@/page/home/index.scss";
-
 export function Home() {
   const [greetMessage, setGreetMessage] = useState("");
   const [name, setName] = useState("");
   const greetApi = generateApiInstance("greet");
   const [count, setCount] = useState(0);
-  // Effect<void>
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   const task = useMemo(
     () => Effect.sync(() => setCount((current) => current + 1)),
-    [setCount],
+    [],
   );
   const increment = useCallback(() => Effect.runSync(task), [task]);
 
   async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMessage(await greetApi({ name }));
   }
 
