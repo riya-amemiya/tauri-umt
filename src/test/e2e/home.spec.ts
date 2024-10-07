778a4ffe2b41f1e9617be950eb52bb9e13fd6c80
Yuta Nishi <riyaamemiya@gmail.com>
2024-10-07 23:40:06 +0900

    test: E2Eテストに入力とボタンクリックの検証を追加

--- src/test/e2e/home.spec.ts
+++ src/test/e2e/home.spec.ts
@@ -7,19 +7,26 @@ test("has title", async ({ page }) => {
 
 test("typing in the input and clicking the greet button", async ({ page }) => {
   await page.goto("http://localhost:9000/");
+
   const usernameLabel = page.getByTestId("username-label");
   expect(await usernameLabel.textContent()).toBe("Username");
+
   const usernameDescription = page.getByTestId("username-description");
   expect(await usernameDescription.textContent()).toBe(
     "This is your public display name.",
   );
+
   const usernameInput = page.getByTestId("username-input");
   expect(await usernameInput.getAttribute("placeholder")).toBe("shadcn");
+
   await usernameInput.click();
   await page.keyboard.type("test");
+
   const greetButton = page.getByTestId("greet-button");
   expect(await greetButton.textContent()).toBe("Greet");
+
   await greetButton.click();
   const greetMessage = page.getByTestId("greet-message");
+
   expect(await greetMessage.textContent()).toBe("Not running in Tauri.");
 });
