a41fd57413223002712004c2a6db6fa971bab7a9
Yuta Nishi <riyaamemiya@gmail.com>
2024-10-09 14:56:24 +0900

    refactor(storybook): Storybook configurationをNext.jsに移行し、エイリアスを設定する

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,39 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/nextjs";
+
+import path from "node:path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-viewport",
+    {
+      name: "storybook-addon-next",
+      options: {
+        nextConfigPath: path.resolve(__dirname, "../next.config.js"),
+      },
+    },
   ],
+  webpackFinal: (config) => {
+    if (config.resolve) {
+      // typescriptのエイリアスをstorybookでも使えるようにする
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+    return config;
+  },
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/nextjs",
     options: {},
   },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,9 @@
-import "the-new-css-reset/css/reset.css";
-import "../src/style/tailwind.css";
-
 import type { Preview } from "@storybook/react";
-
+import "../src/style/tailwind.css";
+import "the-new-css-reset/css/reset.css";
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
